0ff6b4d577d878bfddda9370d59585d50e661bd5
arunjangir <jangirarun786@gmail.com>
2022-02-25 20:19:21 +0300

    List implmented

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -2,22 +2,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
+    tags: ["Arun", "Varun", "Hello"],
   };
 
   styles = {
     fontSize: 50,
     fonWeight: "bold",
   };
-
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
-      let classes="badge m-2 bg-"
-      classes += this.state.count === 0 ? "warning" : "primary";
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
-        <span className={classes}>
-          {this.formatCount()}
-        </span>
+        <span className={classes}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.renderTags()}
       </div>
     );
   }
