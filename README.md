7d6cd9e0a4ea8fba513763aee8ce5523a4f8a8ea
arunjangir <jangirarun786@gmail.com>
2022-02-25 19:10:43 +0300

    Hello-Web

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # React-Tutorial
 First Time learning React.js
+
+## Steps
+
+```
+npm.cmd i -g create-react-app
+create-react-app.ps1 react-app
+```
+
+
+## VS-Code extensions
+
+* Simple React Snippets
---
+++ react-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = <h1>Hello Web Multiverse</h1>
+console.log(element)
+ReactDOM.render(element,document.getElementById('root'))
