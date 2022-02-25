dad0ed8731a0986b9985cad097025c9edc1dd3da
arunjangir <jangirarun786@gmail.com>
2022-02-25 20:32:06 +0300

    implemented event handeling

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
     tags: ["Arun", "Varun", "Hello"],
   };
 
-  styles = {
-    fontSize: 50,
-    fonWeight: "bold",
-  };
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
     return (
@@ -19,13 +15,24 @@ class Counter extends Component {
       </ul>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+  };
+  
   render() {
     let classes = "badge m-2 bg-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
         <span className={classes}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         {this.renderTags()}
       </div>
     );
