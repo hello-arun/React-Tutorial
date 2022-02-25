d817c565529be10e1fd61fe252da968fe205d13d
arunjangir <jangirarun786@gmail.com>
2022-02-25 19:31:02 +0300

    Button Added

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     render() { 
-        return <h1>Hello First Component</h1>;
+        return (
+        <React.Fragment>
+            <h1>Hello First Component</h1>
+            <button>Do not Clcik Me</button>
+        </React.Fragment>
+        )
     }
-}
- 
+} 
 export default Counter;
